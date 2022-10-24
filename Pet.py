aeb15009ba0df934841780ff09d4273cf3da92dc
I AM TON <iamtoncashstation@gmail.com>
2022-10-24 21:49:34 +0700

    Pet

--- Pet.py
+++ Pet.py
@@ -5,11 +5,6 @@ p4 = [int(x) for x in input().split()]
 p5 = [int(x) for x in input().split()]
 all = [p1, p2, p3, p4, p5]
 sol = []
-max_sol = 0
 for i in all:
-    sol.append([sum(i)])
-max_in = sol.index(max(sol))+1
-sol.sort()
-for j in sol[-1]:
-    max_sol += int(j)
-print(max_in, max_sol)
+    sol.append(sum(i))
+print(sol.index(max(sol))+1, max(sol))
